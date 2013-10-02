6daa3babd09bc7e57b2ddc117257aa91f17b137c
Richard Soutar <richard.soutar@gmail.com>
2013-10-02 19:09:06 +0700

    update Ruby Version

--- Vagrantfile
+++ Vagrantfile
@@ -5,6 +5,8 @@ Vagrant::Config.run do |config|
 
   config.vm.forward_port 3000, 3000
 
+  config.vm.share_folder "palgo-site", "/var/palgo-site", "C:\\Users\\Richard\\Documents\\GitHub\\palgo_site"
+
   config.vm.provision :puppet,
     :manifests_path => 'puppet/manifests',
     :module_path    => 'puppet/modules'
--- puppet/manifests/default.pp
+++ puppet/manifests/default.pp
@@ -70,7 +70,7 @@ class install-rvm {
     'ruby-1.9.3-p194':
       ensure => 'present',
       default_use => false;
-    'ruby-1.8.7-p370':
+    'ruby-2.0.0-p247':
       ensure => 'present',
       default_use => false;
   }
@@ -79,9 +79,9 @@ class install-rvm {
     'ruby-1.9.3-p194/bundler': ensure => latest;
     'ruby-1.9.3-p194/rails': ensure => latest;
     'ruby-1.9.3-p194/rake': ensure => latest;
-    'ruby-1.8.7-p370/bundler': ensure => latest;
-    'ruby-1.8.7-p370/rails': ensure => latest;
-    'ruby-1.8.7-p370/rake': ensure => latest;
+    'ruby-2.0.0-p247/bundler': ensure => latest;
+    'ruby-2.0.0-p247/rails': ensure => latest;
+    'ruby-2.0.0-p247/rake': ensure => latest;
   }
 
 }
